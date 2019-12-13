d4c61516a68f3fbc3b4d0be3bfa89e7d6714b270
Dave Campos <dacamposc96@gmail.com>
2019-12-13 05:49:28 +0000

    SCCS

--- spec/controllers/nasa_pics_controller_spec.rb
+++ spec/controllers/nasa_pics_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe NasaPicsController, type: :controller do
 
-  describe "nasa_pics#apod_show" do
+  describe "nasa_pics#show" do
     it "should successfully show apod on the page" do
-      get :apod_show
+      get :show
       expect(response).to have_http_status(:success)
     end
   end
--- spec/models/nasa_api_spec.rb
+++ spec/models/nasa_api_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe NasaApi, type: :model do
+# RSpec.describe NasaApi, type: :model do
   
-end
+# end
